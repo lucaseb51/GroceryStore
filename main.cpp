227b4b179fb3c2e748539d887ed6fcd3d2a7d4fa
Lucas G Eastburn <leastburn@mail.csuchico.edu>
2018-02-23 15:47:41 -0800

    List fully works

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
+//Author: Lucas Eastburn
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
+  int size=0;//where at in the array
   string list[5]; //array of 5 strings
-  int numItems = 0;
   char input;
+  int tf=0;
+  string item;
+
+
+while(tf==0){
 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
@@ -15,5 +21,27 @@ int main()
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
 
+if(input=='q'||input=='Q')
+    {
+        tf=1;
+    }
+
+if(input=='a'||input=='A')
+{
+    if(size<5)
+    {
+    cout<<"Name an item you would like to add to your list."<<endl;
+    cin>>item;
+    list[size]=item;
+    size++;
+    }else
+        {
+        cout<<"You'll need a bigger list!"<<endl;
+        }   
+
+
+
+}
+}
   return 0;
 }
